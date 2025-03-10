6ec55cc1bf61a68fa013f8eb6ed4ea1ec92881ce
DELL PRO <wanflous@gmail.com>
2025-03-10 09:06:12 +0000

    ajouter test file

---
+++ src/test/java/org/exemple/MainTest.java
@@ -0,0 +1,13 @@
+package org.exemple;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+    @Test
+    public void mainTest(){
+        assertEquals(1+1, 2);
+    }
+
+}
